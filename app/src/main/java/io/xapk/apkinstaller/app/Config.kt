ee03611f4b4c2ac0c01c9b25534a4730b27b409e
xiongke <xiongke@apkpure.com>
2019-09-19 17:02:51 +0800

    no message

--- app/src/main/java/io/xapk/apkinstaller/app/Config.kt
+++ app/src/main/java/io/xapk/apkinstaller/app/Config.kt
@@ -1,9 +1,15 @@
 package io.xapk.apkinstaller.app
 
+import io.xapk.apkinstaller.R
+
 /**
  * @author xiongke
  * @date 2019-09-19
  */
 object Config {
     const val XAPK_VERSION = 2
+}
+
+object Settings {
+    val languageValue by lazy { App.mContext.getString(R.string.language_auto_value) }
 }
--- app/src/main/java/io/xapk/apkinstaller/utils/Settings.kt
+++
@@ -1,8 +0,0 @@
-package io.xapk.apkinstaller.utils
-
-import io.xapk.apkinstaller.R
-import io.xapk.apkinstaller.app.App
-
-object Settings {
-    val languageValue by lazy { App.mContext.getString(R.string.language_auto_value) }
-}
